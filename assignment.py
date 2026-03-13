8d214c2330ee685ea96abf01a5812c15ee9a1ff2
Karimi Eliud Maina <eliudkarimi20@gmail.com>
2026-03-13 12:08:40 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,25 @@
+#Question 4
+print("Hello AI Students")
 
+#Question 5
+name = "John"
+age = 20
+city = "Nairobi"
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+
+#Question 6
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+
+print(languages[0])
+
+#Question 7
+student = {
+    "name": "John",
+    "age": 21,
+    "course": "AI"
+}
+
+print(student["name"])
